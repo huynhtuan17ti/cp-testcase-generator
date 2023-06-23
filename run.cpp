5df0e40ae9cd139835d25b6404b9ce4709033542
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2023-06-24 00:10:44 +0700

    fix strcmp

--- run.cpp
+++ run.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(int argc, char** argv){
     int num_test_case = 10;
-    if(argc == 3 && strcmp(argv[1], "-N")){
-        num_test_case = stoi(argv[2]);
+    if(argc == 3 && strcmp(argv[1], "-N") == 0){
+        num_test_case = stoi((string)argv[2]);
         testGenerator(num_test_case);
     }
-    else if(argc == 2 && strcmp(argv[1], "-temp")){
+    else if(argc == 2 && strcmp(argv[1], "-temp") == 0){
         system("cp /usr/bin/gen_temp/rand.cpp rand.cpp");
         system("cp /usr/bin/gen_temp/random.h random.h");
     }else{
